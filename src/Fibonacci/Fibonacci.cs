3697adc82f1e2756754a0258914d4e2099eba203
NathanS-STCU <nathans@stcu.org>
2025-04-17 17:07:42 -0700

    initial project and tests

---
+++ src/Fibonacci/Fibonacci.cs
@@ -0,0 +1,12 @@
+﻿namespace Fib
+{
+    public static class Fibonacci
+    {
+        public static int CalculateFibIndex(int n)
+        {
+            if (n <= 0) throw new ArgumentException("Input must be a non-negative integer.");
+
+            return 0;
+        }
+    }
+}
---
+++ src/FibonacciTests/FibonacciTests.cs
@@ -0,0 +1,15 @@
+﻿using Fib;
+using NUnit.Framework;
+
+namespace FibonacciTests;
+
+[TestFixture]
+public class FibonacciTests
+{
+    [TestCase(-1)]
+    [TestCase(0)]
+    public void ShouldThrowExceptionWhenNonPositiveNumber(int testInput)
+    {
+        Assert.Throws<ArgumentException>(() => Fibonacci.CalculateFibIndex(testInput));
+    }
+}
